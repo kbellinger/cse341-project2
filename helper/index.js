411d69e30d5bfd1c87565747bebc1a1c6d2731b1
kbellinger <buc18003@byui.edu>
2023-02-25 23:16:16 -0700

    joi fixes

--- helper/index.js
+++ helper/index.js
@@ -21,7 +21,7 @@ function validateBook(book){
         publisher: Joi.string().required(),
         pages: Joi.string().required(),
         bestSellerList: Joi.string().required(),
-        totalInSeries: Joi.number().required()
+        totalInSeries: Joi.number().integer().min(1).required()
     })
     .options({ abortEarly: false });
 
